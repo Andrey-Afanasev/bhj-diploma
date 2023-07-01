cf6e4e9aa9d46241980106d43707cdcf49bc19e5
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-07-01 20:37:55 +0700

    1,07

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,54 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
+const createRequest = (options = {}) => { // как тут написать пинимаемые значения?
+    const xhr = new XMLHttpRequest,
+    formData = new FormData;
+    xhr.responseType = 'json';
+    
+    if ('тут нужно написать, если(запрос GET, то выполняем то что ниже)') {
+        const funcun =() => {
+
+            if(xhr.readyState === xhr.DONE) {
+                callback(xhr.response ) 
+                
+            } else {
+                callback(err)
+            }
+    
+        }
+        
+
+        xhr.open( 'GET', 'https://example.com?mail=ivan@biz.pro&password=odinodin' ); // тут в ссылке mail и password, берутся значения которые принимаютсяcreateRequest?
+        xhr.send();
+
+        xhr.addEventListener('readystatechange', funcun)
+
+
+    } else { //'тут соответственно если не GET
+
+        const funcun =() => {
+
+            if(xhr.readyState === xhr.DONE) {
+                callback(xhr.response ) 
+                
+            } else {
+                callback(err)
+            }
+    
+        }
+
+
+
+        formData.append( 'mail', 'ivan@biz.pro' ); // тут в ссылке mail и password, берутся значения которые принимаютсяcreateRequest?
+        formData.append( 'password', 'odinodin' );
+
+        xhr.open( 'POST', 'https://example.com' );
+        xhr.send( formData );
+
+        xhr.addEventListener('readystatechange', funcun)
+    }
+
+  
 
 };
